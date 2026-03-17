ee298d6bfebbd8e8ea4e6c4032878f0981ecea52
dipakligade <ligadedipak8@gmail.com>
2026-03-17 23:59:49 +0530

    Update ToastContainer theme to 'light' in AuthLayout and DashboardLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -55,7 +55,7 @@ export const AuthLayout: React.FC<AuthLayoutProps> = ({ children, title, subtitl
                 </div>
             </div>
 
-            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" />
+            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
         </div>
     );
 };
